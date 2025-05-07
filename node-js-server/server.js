4612917ac15682952f70041ea68b7c3f64fe4f2d
Felix <felix.leuchtmann@web.de>
2025-05-07 09:07:33 +0000

    node-js-server

---
+++ node-js-server/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'serveruser',
+  password: 'server',
+  database: 'webshop'
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySQL Connected...');
+});
+
+app.get('/data', (req, res) => {
+  let sql = 'SELECT * FROM products';
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+app.get('/products', (req, res) => {
+  let sql = 'SELECT * FROM products';
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+app.get('/category', (req, res) => {
+  let sql = 'SELECT * FROM category';
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
