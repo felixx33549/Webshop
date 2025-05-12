3e1e8272d132b14f2f39d24b108bda40b451a707
Felix <felix.leuchtmann@web.de>
2025-05-12 15:26:20 +0200

    update sql backup

--- webshop.sql
+++ webshop.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.41, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.42, for Linux (x86_64)
 --
 -- Host: localhost    Database: webshop
 -- ------------------------------------------------------
--- Server version	8.0.41-0ubuntu0.24.04.1
+-- Server version	8.0.42-0ubuntu0.24.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -138,7 +138,7 @@ CREATE TABLE `products` (
   PRIMARY KEY (`id`),
   KEY `cat_id` (`cat_id`),
   CONSTRAINT `products_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `category` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -147,7 +147,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Intel Core Ultra 7 265K','Intel Prozessor der neusten Generation',419.99,1,100),(2,'Intel Core Ultra 9 285K','Intel Prozessor der neusten Generation',649.99,1,50),(3,'Intel Core Ultra 5 245K','Intel Prozessor der neusten Generation',299.99,1,150),(4,'AMD Ryzen 7 9800X3D','AMD Prozessor der neusten Generation mit 3DV-Cache',499.99,1,150),(5,'AMD Ryzen 9 9950X3D','AMD Prozessor der neusten Generation mit 3DV-Cache',699.99,1,150),(6,'AMD Ryzen 9 9900X3D','AMD Prozessor der neusten Generation mit 3DV-Cache',599.99,1,150);
+INSERT INTO `products` VALUES (1,'Intel Core Ultra 7 265K','Intel Prozessor der neusten Generation',419.99,1,100),(2,'Intel Core Ultra 9 285K','Intel Prozessor der neusten Generation',649.99,1,50),(3,'Intel Core Ultra 5 245K','Intel Prozessor der neusten Generation',299.99,1,150),(4,'AMD Ryzen 7 9800X3D','AMD Prozessor der neusten Generation mit 3DV-Cache',499.99,1,150),(5,'AMD Ryzen 9 9950X3D','AMD Prozessor der neusten Generation mit 3DV-Cache',699.99,1,150),(6,'AMD Ryzen 9 9900X3D','AMD Prozessor der neusten Generation mit 3DV-Cache',599.99,1,150),(7,'ASUS ROG Strix Z890-E Gaming WIFI','Intel Mainboard mit Z890 Chipsatz. LGA1851 Sockel. ATX Formfaktor.',529.00,2,50),(8,'ASUS ROG Maximus Z890 HERO','Intel Mainboard mit Z890 Chipsatz. LGA1851 Sockel. ATX Formfaktor.',739.00,2,40),(9,'ASUS ROG Maximus Z890 EXTREME','Intel Mainboard mit Z890 Chipsatz. LGA1851 Sockel. E-ATX Formfaktor.',1329.00,2,10),(10,'MSI MEG Z890 Godlike','Intel Mainboard mit Z890 Chipsatz. LGA1851 Sockel. E-ATX Formfaktor',1219.00,2,10),(11,'MSI MPG Z890 Carbon WIFI','Intel Mainboard mit Z890 Chipsatz. LGA1851 Sockel. ATX Formfaktor',489.00,2,50),(12,'MSI MEG Z890 Unify-X','Intel Mainboard mit Z890 Chipsatz. LGA1851 Sockel. ATX Formfaktor',699.00,2,50),(13,'Corsair DIMM 64GB DDR5-6400 (2x 32GB) Dual-Kit','Corsair Vengeance DDR5 Arbeitsspeicher mit Intel XMP Profil.',259.00,3,150),(14,'Corsair DIMM 32GB DDR5-6400 (2x 16GB) Dual-Kit','Corsair Vengeance DDR5 Arbeitsspeicher mit Intel XMP Profil.',129.00,3,150),(15,'SAMSUNG 990 PRO 1 TB','Samsung M.2 PCIe SSD. PCIe 4.0 x4, NVMe 2, M.2 2280',109.00,4,200),(16,'SAMSUNG 990 PRO 2 TB','Samsung M.2 PCIe SSD. PCIe 4.0 x4, NVMe 2, M.2 2280',179.00,4,200),(17,'SAMSUNG 9100 PRO 2 TB','Samsung M.2 PCIe SSD. PCIe 5.0 x4, NVMe 2, M.2 2280',269.00,4,100),(18,'SAMSUNG 980 PRO 2 TB','Samsung M.2 PCIe SSD. PCIe 4.0 x4, NVMe 1.3c, M.2 2280',109.00,4,200),(19,'Corsair MP700 Pro 2 TB','Corsair M.2 PCIe SSD. PCIe 5.0 x4, NVMe 2, M.2 2280',109.00,4,200),(20,'Corsair NAUTILUS 360 RS','AIO Wasserkühlung mit 360mm Radiator',105.00,5,50),(21,'Corsair NAUTILUS 360 RS ARGB','AIO Wasserkühlung mit 360mm Radiator',134.90,5,50),(22,'Corsair 6500X','ATX Tower-Gehäuse mit Tempered Glass',236.00,6,50),(23,'Lian Li O11 Dynamic EVO RGB','ATX Tower-Gehäuse mit Seitenteil aus Glas',179.00,6,50),(24,'Corsair 4000D Airflow','ATX Tower-Gehäuse mit Tempered Glass',99.00,6,70),(25,'Corsair iCUE 5000D RGB Airflow','ATX Tower-Gehäuse mit Tempered Glass und RGB Controller',199.00,6,50),(26,'Corsair RM850x','850 Watt Netzteil. 80+ Gold Zertifizierung',140.00,7,150),(27,'Corsair RM1000x','1000 Watt Netzteil. 80+ Gold Zertifizierung',180.00,7,150),(28,'MSI GeForce RTX 5070 12G GAMING TRIO OC','NVIDIA RTX Grafikkarte der neusten Generation.',709.00,8,50),(29,'MSI GeForce RTX 5090 32G GAMING TRIO OC','NVIDIA RTX Grafikkarte der neusten Generation.',2799.00,8,9),(30,'MSI GeForce RTX 5080 GAMING TRIO OC','NVIDIA RTX Grafikkarte der neusten Generation.',1399.00,8,0),(31,'MSI GeForce RTX 5090 32G VANGUARD SOC LAUNCH EDITION','NVIDIA RTX Grafikkarte der neusten Generation.',3199.00,8,0);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -160,4 +160,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-06 13:18:51
+-- Dump completed on 2025-05-12 13:22:37
